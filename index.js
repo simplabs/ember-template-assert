65a156b04f193fe5b919b4cde0a1cc7cce9af79a
nickschot <nickschot@gmail.com>
2022-07-01 17:03:23 +0200

    implement ast-transform that removes assert helper usages from production helpers

--- index.js
+++ index.js
@@ -2,4 +2,31 @@
 
 module.exports = {
   name: require('./package').name,
+
+  options: {
+    babel: {
+      plugins: [require.resolve('ember-auto-import/babel-plugin')],
+    },
+  },
+
+  setupPreprocessorRegistry(type, registry) {
+    const plugin = this._buildPlugin();
+    plugin.parallelBabel = {
+      requireFile: __filename,
+      buildUsing: '_buildPlugin',
+      params: {},
+    };
+    registry.add('htmlbars-ast-plugin', plugin);
+  },
+
+  _buildPlugin() {
+    const emberTemplateAssertTransform = require('./lib/ast-transform');
+
+    return {
+      name: 'ember-template-assert',
+      plugin: emberTemplateAssertTransform,
+      baseDir: emberTemplateAssertTransform.baseDir,
+      cacheKey: emberTemplateAssertTransform.cacheKey,
+    };
+  },
 };
