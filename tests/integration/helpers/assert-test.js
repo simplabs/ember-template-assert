9f697661851b1c3c736c9ca6c56911f68c5bcfb3
nickschot <nickschot@gmail.com>
2022-07-08 10:39:30 +0200

    add integration tests for the assert template helper

---
+++ tests/integration/helpers/assert-test.js
@@ -0,0 +1,42 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'dummy/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import Ember from 'ember';
+
+module('Integration | Helper | assert', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.emberOnError = Ember.onerror;
+  });
+
+  hooks.afterEach(function() {
+    Ember.onerror = this.emberOnError;
+  });
+
+  test('it renders normally when the assertion is truthy', async function (assert) {
+    assert.expect(1);
+
+    Ember.onerror = () => {
+      assert.ok(false);
+    };
+
+    await render(hbs`{{assert "Some assertion message" true}}`);
+
+    assert.ok(true);
+  });
+
+  test('it fails to render when the assertion is falsy', async function (assert) {
+    assert.expect(1);
+
+    Ember.onerror = (error) => {
+      assert.strictEqual(
+        error.message,
+        'Assertion Failed: Some assertion message'
+      );
+    };
+
+    await render(hbs`{{assert "Some assertion message" false}}`);
+  });
+});
